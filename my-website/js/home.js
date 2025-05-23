4964fab62599d4e7db001d5c0ca4e35aa2f9f233
Philic19 <68896939+Philic19@users.noreply.github.com>
2025-05-23 22:13:27 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = '277256e815b05aae4f56dd5dd45eaa97';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
