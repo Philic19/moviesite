5d3e4a8041a7f6500dfaa48c105c4369c185e343
Philic19 <68896939+Philic19@users.noreply.github.com>
2025-05-24 15:20:29 +0800

    Update tvshows.js

--- my-website/js/tvshows.js
+++ my-website/js/tvshows.js
@@ -15,7 +15,7 @@ const mediaType = 'tvShows'; // 🔁 CHANGED from 'movie' to 'tv'
 
 async function fetchLatestMovies(page = 1) {
   try {
-    const res = await fetch(`${BASE_URL}/trending/${mediaType}/week?api_key=${API_KEY}&page=${page}`);
+    const res = await fetch(`https://api.themoviedb.org/3/trending/tv/week?api_key=${API_KEY}&page=${page}`);
     const data = await res.json();
 
     currentPage = page;
