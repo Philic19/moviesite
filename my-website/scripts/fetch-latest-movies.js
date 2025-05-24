bf18c6ebfac3d3ef306de703a945206a157b832e
Philic19 <68896939+Philic19@users.noreply.github.com>
2025-05-24 14:59:18 +0800

    Update fetch-latest-movies.js

--- my-website/scripts/fetch-latest-movies.js
+++ my-website/scripts/fetch-latest-movies.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 const fs = require('fs');
 const path = require('path');
 
-const API_KEY = 'your_tmdb_api_key'; // Use your actual TMDB key here
+const API_KEY = '277256e815b05aae4f56dd5dd45eaa97'; // Use your actual TMDB key here
 const URL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`;
 
 (async () => {
